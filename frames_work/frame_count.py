19db1b7fd787e06ff762fd76b93b00561e753ec1
mangesh mane <mangeshmane.00009@gmail.com>
2021-12-17 16:40:23 +0530

    complet the documation and formatting

--- frames_work/frame_count.py
+++ frames_work/frame_count.py
@@ -1,37 +1,30 @@
 
-
+"""
+Showing frame rate of video
+making the funtion to show video frame rate
+"""
+# import cv and glob 
 import cv2
-import time
-import os
 from glob import glob
 
-source=os.path.join(os.getcwd(),"source_video")
-ls=os.listdir(source)
-for i in ls:
-    pass
-if __name__=="__main__":
-    video=cv2.VideoCapture(i)
+#making the funtion to count frames      
+def frame_count(name):
+    # capture the frame in the video file
+    video=cv2.VideoCapture(name)
+    #showing details of video frame
     (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
-    
+          
     if int(major_ver)<3:
+        #store the frame rate the fps variable 
         fps=video.get(cv2.cv.CV_CAP_PROP_FPS)
-        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(int(fps)))
     else:
         fps=video.get(cv2.CAP_PROP_FPS)
-        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-        
-    num_frames=900
-    print("Capturing {0} frames".format(num_frames))
-    
-    start=time.time()
-    for i in range(0,num_frames):
-        ret,frame=video.read()
-        
-    end=time.time()
-    seconds=end - start
-    print ("Time taken : {0} seconds".format(seconds))
-    
-    fps=num_frames/seconds
-    print("Estimated frames per second : {0}".format(fps))
-    
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(int(fps)))  
+    #all condition true than close the video   
     video.release()
+
+if __name__=="__main__":
+      source_path=glob("source_video/*")
+      for path in source_path:
+        frame_count(path)
