fa23a006b8c833a833f5ce9c6d04bde206104aea
mangesh mane <mangeshmane.00009@gmail.com>
2021-12-17 17:45:12 +0530

    add loader in output

--- frames_work/main.py
+++ frames_work/main.py
@@ -8,6 +8,8 @@
 import cv2
 import os
 from glob import glob
+from time import sleep
+import sys
 from frame_count import frame_count
 
 #making the function for creating direactory
@@ -20,25 +22,32 @@ def createdire(path):
         
 #making a function for generate the frame through video
 def genarate_frame(path,save_path,gap):
-    name=path.split("/")[-1].split(".")[0] # extract the name of video for video
-    save_path=os.path.join(save_path,name) # define the path of saving 
-    
-    createdire(save_path)# call the function for create the save direactory
-    
-    cap=cv2.VideoCapture(path)
-    idx=0
-    while True:
-        res,frame=cap.read()
-        if res == False:
-            cap.release()
-            break
-        if idx ==0:
-            cv2.imwrite(f"{save_path}/{idx}.jpg",frame)
-        else:
-            if idx % gap == 0:
-                cv2.imwrite(f"{save_path}/{idx}.jpg",frame)
-        idx+=1
-
+            name=path.split("/")[-1].split(".")[0] # extract the name of video for video
+            save_path=os.path.join(save_path,name) # define the path of saving 
+          
+            createdire(save_path)# call the function for create the save direactory
+            for z in range(21):
+                  sleep(1.0)
+                  sys.stdout.write("\r")
+                  cap=cv2.VideoCapture(path)
+                  idx=0
+                  while True:
+                        res,frame=cap.read()
+                        if res == False:
+                              cap.release()
+                              break
+                        if idx ==0:
+                              cv2.imwrite(f"{save_path}/{idx}.jpg",frame)
+                        else:
+                              if idx % gap == 0:
+                                    cv2.imwrite(f"{save_path}/{idx}.jpg",frame)
+                        idx+=1
+                  sys.stdout.write("[%-20s]%d%%" %( "="* z,5*z) )
+            sys.stdout.flush()
+            sleep(0.2)
+            print("\n")
+            print(f"Video to Frame converting done frame rate is {gap}")
+            print("\n")
 
 
 if __name__=="__main__":
