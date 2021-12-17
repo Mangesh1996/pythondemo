a4e7072012d8576bb02d4705e2bb35d2b899285e
mangesh mane <mangeshmane.00009@gmail.com>
2021-12-17 11:58:16 +0530

    convert frame to video

---
+++ frames_work/frame_video.py
@@ -0,0 +1,17 @@
+import numpy as np
+import glob
+import cv2
+
+
+img_array=[]
+
+for filename in glob.glob("/home/diycam/Desktop/dump/frames_work/save_frame/ironman/*.jpg"):
+    img=cv2.imread(filename)
+    height,width,layer=img.shape
+    size=(width,height)
+    img_array.append(img)
+    
+out=cv2.VideoWriter("project.avi",cv2.VideoWriter_fourcc(*'DIVX'),15,size)
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
