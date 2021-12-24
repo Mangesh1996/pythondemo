002d06ba17c28bd8a45e9442fae406545abbbb13
mangesh1996 <mangeshmane.00009@gmail.com>
2021-12-24 13:37:51 +0530

    remove gui

--- ffmpeg/demo.py
+++ ffmpeg/demo.py
@@ -18,37 +18,41 @@ import datetime
 from tqdm import tqdm
 from ffmpeg_progress_yield import FfmpegProgress
 from frame_count import frame_count
-# creating exception to handle error
-try:
-    root = tk.Tk()
-    root.withdraw()
-    #specifice the file type when user chose the file
-    filetype=(("Mp4 files","*.mp4"),("Window Media Viewer","*.wvm"),("Audio Video Interleave","*.avi"),)
-    #make a default path to show user in dialog box
-    defalut_path=os.path.join(os.getcwd())
-    user_fps=frame_count("source_video/ironmane_fly.mp4")
-    #save the path when user chose path using dialog box
-    file_path=filedialog.askopenfile(title="Select Video to convert frame",initialdir=defalut_path,filetypes=filetype)
-    #select only path name in filedialog box
-    sourcepath=file_path.name
-    #save path in save_path 
-    save_path=filedialog.askdirectory(title="Select Folder to save frames")
-    #run the command using os.system 
-    start =time.time()
-    fp=frame_count("source_video/ironmane_fly.mp4")
-    cmd = [
-    "ffmpeg", "-i", "source_video/ironmane_fly.mp4", "-vf", "fps=1", "frame_dire/img-%03d.jpg",]
-    cmd[4]="fps="+str(fp)
+from glob import glob
 
-    ff = FfmpegProgress(cmd)
-    with tqdm(total=100, position=0, desc="Converting....") as pbar:
-        for progress in ff.run_command_with_progress():
-            pbar.update(progress - pbar.n)
+def covert_video_frame(path,save_path,frame):
+    # creating exception to handle error
+    try:
+        name=path.split("/")[-1].split(".")[0]     
+        save_path=os.path.join(save_path,name)
+        start = time.time()
+        
+        cmd = [
+            "ffmpeg", "-i", "source_path/ironmane_fly.mp4", "-vf", "fps=1", "frame_dire/img-%03d.jpg", ]
+        cmd[4]="fps="+frame
+        
+        cmd[5] = save_path+"/img-%03d.jpg"
+        uni = [0, ]
+        ff = FfmpegProgress(cmd)
+        with tqdm(total=100, position=0, desc="Converting....") as pbar:
+            for progress in ff.run_command_with_progress():
+                pbar.update(progress - pbar.n)
+                uni.insert(1, progress)
+                uni.pop(0)
+                print(uni)
+
+        end = time.time()
+        print("\n")
+        print("Video to frame converting done ")
+        sec = end-start
+        print("task complete duration:- ", str(datetime.timedelta(seconds=sec)))
+    except Exception:
+        print("Your are Not select properly file ")
+
+
+if __name__=="__main__":
     
-    end=time.time()
-    print("\n")
-    print("Video to frame converting done ")
-    sec=end-start
-    print("task complete duration:- ",str(datetime.timedelta(seconds=sec)))
-except Exception:
-    print("Your are Not select properly file ")
+    sourcepath=glob("source_path/*")
+    save_path="save"
+    for path in sourcepath:
+        covert_video_frame(path,save_path,1)
