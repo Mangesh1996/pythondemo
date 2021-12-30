9843ad8ac58948583b59b3a056299d8f306736c7
mangesh1996 <mangeshmane.00009@gmail.com>
2021-12-30 17:14:52 +0530

    updated

--- image_scraping/test.py
+++ image_scraping/test.py
@@ -1,4 +1,5 @@
 import os
+from re import search
 import selenium
 from selenium import webdriver
 import time
@@ -8,54 +9,84 @@ import requests
 from webdriver_manager import driver
 import webdriver_manager
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.opera import OperaDriverManager
 from selenium.common.exceptions import ElementClickInterceptedException, ElementNotInteractableException
 
-
-# install chrome drive  and luanch the chrome browser
+# opts=webdriver.ChromeOptions()
+# opts.headless=True
 driver=webdriver.Chrome(ChromeDriverManager().install())
 
-#url for google image section
-search_url="https://www.google.com/search?q={q}&client=opera&hs=y9r&source=lnms&tbm=isch&sa=X&ved=2ahUKEwit3ceT6or1AhVHFogKHfKvC0gQ_AUoAXoECAEQAw&biw=1813&bih=952&dpr=1"
 
-#query for user define
-# query=input("Enter the search keworkd")
-#get query of google page
-driver.get(search_url.format(q='car'))
+def scroll_to_end(driver):
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    time.sleep(2)
 
-# driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-# time.sleep(5)
+def getImageUrls(name,totalimage,driver):
+    search_url="https://www.google.com/search?q={q}&client=opera&hs=tDE&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiCnv-R_or1AhUNE4gKHaTLAVkQ_AUoAXoECAEQAw&biw=1813&bih=952&dpr=1"
+    driver.get(search_url.format(q=name))
+    img_urls=set()
+    img_count=0
+    result_start=0
+    while(img_count<totalimage):# extract actual image
+        scroll_to_end(driver)
+        thumbnail_result=driver.find_elements_by_xpath("//img[contains(@class,'Q4LuWd')]")
+        totalresults=len(thumbnail_result)
+        print(f"Found: {totalresults} search result , Extracting line from {result_start}:{totalresults}")
+        for img in thumbnail_result[result_start:totalresults]:
+            img.click()
+            time.sleep(2)
+            actual_images=driver.find_elements_by_css_selector('img.n3VNCb')
+            for actual_image in actual_images:
+                if actual_image.get_attribute('src') and 'https' in actual_image.get_attribute('src'):
+                    img_urls.add(actual_image.get_attribute('src'))
+            img_count=len(img_urls)
+            if img_count >= totalimage:
+                # if img_count % 20 ==0:
+                #     continue
+                print(f"Found : {img_count} image links")
+                break
+            else:
+                print("Found: ",img_count,"Looking for more image links ...")
+                load_more_button=driver.find_element_by_css_selector(".mye4qd")
+                driver.execute_script("document.querySelector('.mye4qd').click();")
+                
+                result_start=len(thumbnail_result)
+    return img_urls
+# getImageUrls("cat",10,driver)
 
-imgresult=driver.find_elements_by_xpath("//img[contains(@class,'Q4LuWd')]")
-totalresult=len(imgresult)
-img_url=set()
-for i in range(0,len(imgresult)):
-    img=imgresult[i]
+def downloadImages(folder_path,file_name,url):
     try:
-        img.click()
-        time.sleep(2)
-        actual_img=driver.find_elements_by_css_selector('img.Q4LuWd')
-        for actual in actual_img:
-            if actual.get_attribute('src')and 'https' in actual.get_attribute('src'):
-                img_url.add(actual.get_attribute('src'))
-    except ElementClickInterceptedException or ElementNotInteractableException as err:
-        print(err)
-os.chdir("save")
-baseDir=os.getcwd()
-
-for i ,url in enumerate(img_url):
-    file_name=f"{i}.jpg"
-    try:
-        image_conten=requests.get(url).content
+        image_content=requests.get(url).content
     except Exception as e:
-        print(f"Error - Could Not download {url} - {e}")
+        print(f"Error: Could not Download {url} - {e}")
     try:
-        image_file=io.BytesIO(image_conten)
+        image_file=io.BytesIO(image_content)
         image=Image.open(image_file).convert('RGB')
-        file_path=os.path.join(baseDir,file_name)
-        with open(file_path,'wb')as f:
-            image.save(f,"JPEG",quality=85)
-        print(f"Saved - {url} - AT: {file_path}")
+        file_path=os.path.join(folder_path,file_name)
+        with open(file_path,"wb")as w:
+            image.save(w,"JPEG",quality=85)
+        print(f"saved- {url} - AT: {file_path}")
     except Exception as e:
-        print(f"ERROR - COULD NOT SAVE {url} - {e}")
-        
+        print(f"ERROR - Could Not save {url} - {e}")
+
+def saveInDestFolder(searchname,destdir,totalImage,driver):
+    for name in list(searchname):
+        path=os.path.join(destdir,name)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        print("Current path",path)
+        totallinks=getImageUrls(name,totalImage,driver)
+        print('totalLinks',totallinks)
+        if totallinks is None:
+            print("image not found :-",name)
+            continue
+        else:
+            for i ,link in enumerate(totallinks):
+                file_name=f"{i+1}.jpg"
+                downloadImages(path,file_name,link)
+
+searchNames=['Person'] 
+destDir=f'save'
+totalImgs=300
+
+saveInDestFolder(searchNames,destDir,totalImgs,driver)
+
