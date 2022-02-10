147c1f7ac1b52b85c316ab948c5d212ea7d40636
mangesh1996 <mangeshmane.00009@gmail.com>
2022-02-10 18:41:17 +0530

    update pytube

--- youtub_downloader/main.py
+++ youtub_downloader/main.py
@@ -11,6 +11,7 @@ from time import sleep
 from random import randint
 from find_resoluation import resoulation_find
 import os
+import sys
 
 
 def save_path(download_path):
@@ -22,35 +23,43 @@ def save_path(download_path):
     except OSError:
         print(f"File not created {download_path}")
 #derivate the save path
-download_path=input("Paster the download path:-   ")
-if download_path =="Default" or download_path=="save":
-    save_path(download_path)
-else:
-    download_path
-path=os.path.join(os.getcwd(),download_path)
-# get the input link
-link=input("Paster the Youtube link:-  ")
-#make the try to handle error
-try:
-    yt=YouTube(link)
-    print(yt.title)
-#make the excepthe when the connection issue
-except:
-    print("connection error")
-# call the function for check resoluation
-pixl=resoulation_find(link)
-print("Select Video resoluation :- ")
-for i in range(len(pixl)):
-    print(f"{i+1}."+pixl[i])
-choice=int(input("Choice the resoulation  :- "))
 
-mp4files=yt.streams.filter(res=pixl[choice-1]).first()
-try:
-    # download the video to save path with vide title name
-    mp4files.download(path,filename=f"{yt.title}_{pixl[choice-1]}.mp4")
-except:
-    print("some error")
+def youtube_download(download_path):
+    link=input("Paster the Youtube link:-  ")
+    # download_path=input(f"Paster the download path:-   ")
+    if download_path =="Default" or download_path=="save":
+        save_path(download_path)
+    else:
+        download_path
+    path=os.path.join(os.getcwd(),download_path)
+    # get the input link
+    
+    #make the try to handle error
+    try:
+        yt=YouTube(link)
+        print(yt.title)
+    #make the excepthe when the connection issue
+    except:
+        print("connection error")
+    # call the function for check resoluation
+    pixl=resoulation_find(link)
+    print("Select Video resoluation :- ")
+    for i in range(len(pixl)):
+        print(f"{i+1}."+pixl[i])
+    choice=int(input("Choice the resoulation  :- "))
 
-print(f"downloading complete {yt.title}.mp4")
+    mp4files=yt.streams.filter(res=pixl[choice-1]).first()
+    try:
+        # download the video to save path with vide title name
+        mp4files.download(path,filename=f"{yt.title}_{pixl[choice-1]}.mp4")
+    except:
+        print("some error")
 
-sleep(randint(7,20))
+    print(f"downloading complete {yt.title}.mp4")
+
+    sleep(randint(5,10))
+
+
+if __name__ =="__main__":
+    save="save"
+    youtube_download(save)
